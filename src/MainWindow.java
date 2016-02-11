1ad6862dd7c306b7cfb39ed787c67d20f80a122f
Vincent Hilton <vincentdhilton@gmail.com>
2016-02-11 13:32:26 -0500

    Version 1.5.0 With JPanel

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.awt.Panel;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
@@ -12,10 +13,21 @@ import javax.swing.Box;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainWindow {
 
 	private JFrame frame;
+	Tiles t1;
+	Tiles t2;
+	Tiles t3;
+	Tiles t4;
+	Tiles t5;
+	Tiles t6;
 
 	/**
 	 * Launch the application.
@@ -62,15 +74,35 @@ public class MainWindow {
 		menuBar.add(mnFile);
 		
 		JMenuItem mntmStartGame = new JMenuItem("Start Game");
+		mntmStartGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// call all of our methods to actually run the game
+				// from the TILES class
+				// maybe create the tiles here
+				// run the time method
+				
+				
+			}
+		});
 		mnFile.add(mntmStartGame);
 		
 		JMenuItem mntmReset = new JMenuItem("Reset");
+		mntmReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// reset the game board
+			}
+		});
 		mnFile.add(mntmReset);
 		
 		JMenu mnOptions = new JMenu("Options");
 		menuBar.add(mnOptions);
 		
 		JMenuItem mntmChangeSize = new JMenuItem("Change Size");
+		mntmChangeSize.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// user can change size of grid  EXTRA
+			}
+		});
 		mnOptions.add(mntmChangeSize);
 		
 		JMenuItem mntmChangeImage = new JMenuItem("Change Image");
@@ -81,26 +113,64 @@ public class MainWindow {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// call the tiles method to see what happens when clicked
+
+			}
+		});
+		panel.setBackground(Color.BLACK);
 		panel.setBounds(38, 11, 80, 80);
 		frame.getContentPane().add(panel);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		panel_1.setBackground(Color.BLACK);
 		panel_1.setBounds(170, 11, 80, 80);
 		frame.getContentPane().add(panel_1);
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		panel_2.setBackground(Color.BLACK);
 		panel_2.setBounds(298, 11, 80, 80);
 		frame.getContentPane().add(panel_2);
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		panel_3.setBackground(Color.BLACK);
 		panel_3.setBounds(38, 102, 80, 80);
 		frame.getContentPane().add(panel_3);
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		panel_4.setBackground(Color.BLACK);
 		panel_4.setBounds(170, 102, 80, 80);
 		frame.getContentPane().add(panel_4);
 		
 		JPanel panel_5 = new JPanel();
+		panel_5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		panel_5.setBackground(Color.BLACK);
 		panel_5.setBounds(298, 102, 80, 80);
 		frame.getContentPane().add(panel_5);
 		
---
+++ src/Tiles.java
@@ -0,0 +1,37 @@
+
+public class Tiles {
+	
+	int tileCorrect=0;
+	int tileIncorrect=0;
+	
+	public Tiles(){
+		// image value
+		// size?
+	}
+
+	//meth for changing tile
+	public void tileSelect(){
+	
+		// on click we want this to set image to face
+		System.out.println("Hello");
+		
+		
+	}
+	
+	// meth for failed math
+	public void doesTileMatch(){
+		
+		
+		
+		// after 2 method calls of tile select, we need to see IF MATCH
+		// this is where tenzin if/else logic goes into this meth
+		
+		// if t1 = t2 (lock image, tileCorrect || tileIncorrect 
+		
+		
+		
+	}
+	
+	public void 
+	
+}
